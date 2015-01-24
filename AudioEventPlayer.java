1992b2ace5cf44a499c071bc80b0ee99043911c5
aw1231 <astropiloto@gmail.com>
2015-01-24 13:28:28 -0500

    Added piano sounds and removed midi files

--- AudioEventPlayer.java
+++ AudioEventPlayer.java
@@ -6,38 +6,69 @@ import javax.sound.midi.Sequence;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.InvalidMidiDataException;
 
 import java.io.File;
+
 public class AudioEventPlayer {
-	public static void startNote(int instrument, int note)  throws MidiUnavailableException {
-		Sequencer sequencer;
+	public static void startNote(int instrument, int note)  throws MidiUnavailableException,InvalidMidiDataException {
+        int notevalue = 0;
+        if(instrument == 0){
+            if(note == 0){
+                notevalue = 60;
+            } if(note == 1){
+                notevalue = 64;
+            } if(note == 2){
+                notevalue = 67;
+            }
+        }
+        
+        
+        ShortMessage midinote = new ShortMessage();
+        midinote.setMessage(ShortMessage.NOTE_ON, instrument, notevalue, 93);
+        Receiver rcvr = MidiSystem.getReceiver();
+        rcvr.send(midinote,-1);
+                
+	}
+	public static void endNote(int instrument, int note) throws MidiUnavailableException,InvalidMidiDataException {
+        try{
+        int notevalue = 0;
+        if(instrument == 0){
+            if(note == 0){
+                notevalue = 60;
+            } if(note == 1){
+                notevalue = 64;
+            } if(note == 2){
+                notevalue = 67;
+            }
+        }
+        Receiver rcvr = MidiSystem.getReceiver();
+        ShortMessage midinote = new ShortMessage();
+	    midinote.setMessage(ShortMessage.NOTE_OFF,instrument,notevalue,93);
+        rcvr.send(midinote,-1);
+        } catch (Exception e) {}
+	}
+	public static void main(String[] args) throws MidiUnavailableException,InvalidMidiDataException{
+        Sequencer sequencer;
 	    // Get default sequencer.
 	    sequencer = MidiSystem.getSequencer(); 
 	    if (sequencer == null) {
 	        // Error -- sequencer device is not supported.
-	        // Inform user and return...
-	    	
+	        // Inform user and return...	    	
 	    } else {
-	         // Acquire resources and make operational.
-	    	try {
-	            ShortMessage pianoc = new ShortMessage();
-	            pianoc.setMessage(ShortMessage.NOTE_ON, 0, 60, 93);
-	            ShortMessage pianoend = new ShortMessage();
-	            pianoend.setMessage(ShortMessage.NOTE_OFF,0,60,93);
-	            Receiver rcvr = MidiSystem.getReceiver();
-	            rcvr.send(pianoc,-1);
-				try{Thread.sleep(500);}catch(InterruptedException e){}
-	            rcvr.send(pianoend,-1);
-	            sequencer.open();
-	        } catch (Exception e) {
-	            // Handle error and/or return
-	        }
-	    }
-	}
-	public static void endNote(int instrument, int note) {
-		
-	}
-	public static void main(String[] args) throws MidiUnavailableException{
-		startNote(0,0);
-	}
+	        // Acquire resources and make operational.
+            sequencer.open();
+            startNote(0,0);
+		    try{Thread.sleep(500);}catch(InterruptedException e){}
+            endNote(0,0);
+            try{Thread.sleep(500);}catch(InterruptedException e){}
+            startNote(0,1);
+            try{Thread.sleep(500);}catch(InterruptedException e){}
+            endNote(0,1);
+            try{Thread.sleep(500);}catch(InterruptedException e){}
+            startNote(0,2);
+            try{Thread.sleep(500);}catch(InterruptedException e){}
+            endNote(0,2);
+        }   
+    }
 }
