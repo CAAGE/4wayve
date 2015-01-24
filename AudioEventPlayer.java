4f935e8e577f438512525b4852fe65b04c5d9268
aw1231 <astropiloto@gmail.com>
2015-01-24 14:59:33 -0500

    Added drums and bass

--- AudioEventPlayer.java
+++ AudioEventPlayer.java
@@ -14,7 +14,9 @@ public class AudioEventPlayer {
 	public static void startNote(int instrument, int note)  throws MidiUnavailableException,InvalidMidiDataException {
         Receiver rcvr = MidiSystem.getReceiver();
         int notevalue = 0;
+        int bank = 0;
         if(instrument == 0){
+            bank = 0;
             ShortMessage midiinstrument = new ShortMessage();
             midiinstrument.setMessage(ShortMessage.PROGRAM_CHANGE, 0, 0, 0);
             rcvr.send(midiinstrument,-1);
@@ -26,17 +28,51 @@ public class AudioEventPlayer {
                 notevalue = 67;
             }
         }
+        if(instrument == 1){
+            bank = 9;
+            ShortMessage midiinstrument = new ShortMessage();
+            midiinstrument.setMessage(ShortMessage.PROGRAM_CHANGE,0,0,0);
+            rcvr.send(midiinstrument,-1);
+            if(note == 0){
+                notevalue = 36;
+            }
+            if(note == 1){
+                notevalue = 38;
+            }
+            if(note == 2){
+                notevalue = 49;
+            }
+        }
+        if(instrument == 2){
+            bank = 0;
+            ShortMessage midiinstrument = new ShortMessage();
+            midiinstrument.setMessage(ShortMessage.PROGRAM_CHANGE,0,33,0);
+            rcvr.send(midiinstrument,-1);
+            if(note == 0){
+                notevalue = 36;
+            }
+            if(note == 1){
+                notevalue = 40;
+            }
+            if(note == 2){
+                notevalue = 43;
+            }
+        }
         if(instrument == -1){
+            bank = 0;
             ShortMessage midiinstrument = new ShortMessage();
             midiinstrument.setMessage(ShortMessage.PROGRAM_CHANGE, 0, 115, 0);
             rcvr.send(midiinstrument,-1);
             notevalue = 60;
+        }
         ShortMessage midinote = new ShortMessage();
-        midinote.setMessage(ShortMessage.NOTE_ON, 0, notevalue, 93);
+        midinote.setMessage(ShortMessage.NOTE_ON, bank, notevalue, 93);
         rcvr.send(midinote,-1);
-        }           
+                   
 	}
+
 	public static void endNote(int instrument, int note) throws MidiUnavailableException,InvalidMidiDataException {
+        Receiver rcvr = MidiSystem.getReceiver();
         try{
         int notevalue = 0;
         if(instrument == 0){
@@ -48,10 +84,30 @@ public class AudioEventPlayer {
                 notevalue = 67;
             }
         }
-        Receiver rcvr = MidiSystem.getReceiver();
+        if(instrument == 2){
+            ShortMessage midiinstrument = new ShortMessage();
+            midiinstrument.setMessage(ShortMessage.PROGRAM_CHANGE,0,33,0);
+            rcvr.send(midiinstrument,-1);
+            if(note == 0){
+                notevalue = 36;
+            }
+            if(note == 1){
+                notevalue = 40;
+            }
+            if(note == 2){
+                notevalue = 43;
+            }
+        }
+        if(instrument == -1){
+            ShortMessage midiinstrument = new ShortMessage();
+            midiinstrument.setMessage(ShortMessage.PROGRAM_CHANGE, 0, 115, 0);
+            rcvr.send(midiinstrument,-1);
+            notevalue = 60;
+        }
         ShortMessage midinote = new ShortMessage();
-	    midinote.setMessage(ShortMessage.NOTE_OFF,instrument,notevalue,93);
+	    midinote.setMessage(ShortMessage.NOTE_OFF,0,notevalue,93);
         rcvr.send(midinote,-1);
+        
         } catch (Exception e) {}
 	}
 	public static void main(String[] args) throws MidiUnavailableException,InvalidMidiDataException{
@@ -64,17 +120,17 @@ public class AudioEventPlayer {
 	    } else {
 	        // Acquire resources and make operational.
             sequencer.open();
-            startNote(-1,0);
+            startNote(2,0);
 		    try{Thread.sleep(500);}catch(InterruptedException e){}
-            endNote(-1,0);
+            endNote(2,0);
             try{Thread.sleep(500);}catch(InterruptedException e){}
             startNote(0,1);
             try{Thread.sleep(500);}catch(InterruptedException e){}
             endNote(0,1);
             try{Thread.sleep(500);}catch(InterruptedException e){}
-            startNote(0,2);
+            startNote(1,2);
             try{Thread.sleep(500);}catch(InterruptedException e){}
-            endNote(0,2);
+            endNote(1,2);
         }   
     }
 }
